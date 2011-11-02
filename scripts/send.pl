9db9751c3dbd08fd30dcd85b9084e26c11585792
soh335 <sugarbabe335@gmail.com>
2011-11-02 09:32:16 +0900

    fix scripts/send.pl indent

--- scripts/send.pl
+++ scripts/send.pl
@@ -7,26 +7,26 @@ use AnyEvent;
 use AnyEvent::WebService::ImKayac;
 use Config::Pit;
 
-my %conf = (%{ pit_get('im.kayac') }, type => 'secret');
+my %conf = (%{ pit_get('im.kayac.com') }, type => 'secret');
 
 my $cv = AE::cv;
 
 my $im = AnyEvent::WebService::ImKayac->new(%conf);
 
 $im->send( message => 'Hello! test send', cb => sub {
-    my ($hdr, $json, $err) = @_;
-    
-      if ( $err ) {
-          warn $err;
-      }
-      elsif ( ! $json ) {
-          warn $hdr->{Reason};
-      }
-      elsif ( $json->{result} ne "posted" ) {
-          warn $json->{error};
-      }
-    
-    $cv->send;
-});
+        my ($hdr, $json, $err) = @_;
+
+        if ( $err ) {
+            warn $err;
+        }
+        elsif ( ! $json ) {
+            warn $hdr->{Reason};
+        }
+        elsif ( $json->{result} ne "posted" ) {
+            warn $json->{error};
+        }
+
+        $cv->send;
+    });
 
 $cv->recv;
